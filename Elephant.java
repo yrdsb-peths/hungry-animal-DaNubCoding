a31ef095910a5c9ed86d6507d163d7328f374fa2
DaNub <75505891+DaNubCoding@users.noreply.github.com>
2023-12-01 13:19:16 -0500

    flip elephant

--- Elephant.java
+++ Elephant.java
@@ -2,6 +2,7 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 public class Elephant extends Sprite {
     private GreenfootSound eatSound = new GreenfootSound("elephantcub.mp3");
+    private boolean mirrored = false;
     
     public Elephant(Game game) {
         super(game, 300, 300);
@@ -10,9 +11,17 @@ public class Elephant extends Sprite {
     public void tick() {
         if (Greenfoot.isKeyDown("left")) {
             this.setLocation(getX() - 3, getY());
+            if (!this.mirrored) {
+                this.getImage().mirrorHorizontally();
+                this.mirrored = true;
+            }
         }
         if (Greenfoot.isKeyDown("right")) {
             this.setLocation(getX() + 3, getY());
+            if (this.mirrored) {
+                this.getImage().mirrorHorizontally();
+                this.mirrored = false;
+            }
         }
         
         if (isTouching(Apple.class)) {
