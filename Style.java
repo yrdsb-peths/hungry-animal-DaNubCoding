aa4bf36f65391bccfe4b841240ef239ec5fefa60
DaNub <75505891+DaNubCoding@users.noreply.github.com>
2023-12-06 21:28:51 -0500

    revert Style class to record

--- Style.java
+++ Style.java
@@ -1,30 +1,7 @@
 /**
- * A class that stores the data of a singular style
+ * A reocrd that stores the data of a singular style
+ * 
+ * @param name The name of the field as a string
+ * @param value The value of the field
  */
-public class Style {
-    private String name;
-    private Object value;
-    
-    /**
-     * Initializes the style
-     * 
-     * @param name The name of the field (must be a valid field of a Button if used)
-     * @param value The value of the field
-     */
-    public Style(String name, Object value) {
-        this.name = name;
-        this.value = value;
-    }
-    
-    public String name() {
-        return this.name;
-    }
-    
-    public Object value() {
-        return this.value;
-    }
-    
-    public static Style $(String name, Object value) {
-        return new Style(name, value);
-    }
-}
+public record Style(String name, Object value) {}
